0898dbf57eeabbacdc85f0573f4281b2ef4ca9cb
aikchun <aikchun616@gmail.com>
2017-12-05 22:35:53 +0800

    Lecture 20

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,24 +1,35 @@
 defmodule Cards do
 
-    def create_deck do
-        values = ["Ace", "Two", "Three", "Four", "Five"]
-        suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-        for suit <- suits , val <- values do
-            "#{val} of #{suit}"
-        end
+    for suit <- suits , val <- values do
+      "#{val} of #{suit}"
     end
+  end
 
-    def shuffle(deck) do
-       Enum.shuffle(deck) 
+  def shuffle(deck) do
+    Enum.shuffle(deck) 
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand) do
+    Enum.split(deck, hand)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "this file doesn't exists"
     end
-
-    def contains?(deck, card) do
-        Enum.member?(deck, card)
-    end
-
-    def deal(deck, hand) do
-        Enum.split(deck, hand)
-    end
-
+  end
 end
