d8ece68a0e7b474ea7a802bda80ded6668654967
aikchun <aikchun616@gmail.com>
2017-12-06 11:00:44 +0800

    lecture 22

--- lib/cards.ex
+++ lib/cards.ex
@@ -32,4 +32,10 @@ defmodule Cards do
       {:error, _reason} -> "this file doesn't exists"
     end
   end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
 end
